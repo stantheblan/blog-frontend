63b2f9feba15876e7fd33076908ec9e39d7aeca0
Stan <stantonmoore42@gmail.com>
2022-04-12 21:32:23 -0400

    Create New.jsx

---
+++ src/components/New.jsx
@@ -0,0 +1,25 @@
+import { useNavigate } from 'react-router-dom'
+import { createPost } from '../services/posts-api'
+
+export function New() {
+  let nav = useNavigate()
+  const addNewPost = (e) => {
+    e.preventDefault()
+    let post = {
+      title: e.target.title.value,
+      body: e.target.body.value
+    }
+    createPost(post)
+    nav('/')
+  }
+  return (
+    <div>
+      <h1>Log new location</h1>
+      <form onSubmit={addNewPost}>
+        Location Name <input type='text' name='title' /><br />
+        Hints <br /><textarea name='body' cols='40' rows='10'></textarea><br />
+        <input type='submit' />
+      </form>
+    </div>
+  )
+}
